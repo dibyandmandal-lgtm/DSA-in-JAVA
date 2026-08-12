a95cdce6eb53f31c5783d70705cdcd13d64243c3
dibyandu mandal <dibyandmandal@gmail.com>
2026-08-12 21:36:44 +0530

    without using extra variable

---
+++ sorting/swap.java
@@ -0,0 +1,24 @@
+package sorting;
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
